fabf6ca069252a9c679806d439619d65c2254d98
Roy <debessjes@hotmail.com>
2016-06-30 16:16:18 +0200

    calculate
    When you mention the bot and type any calculation for example :  '@bot:
    calculate  25*4  '. The bot will reply the answer '100'

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ controller.spawn({
   token: process.env.BOT_API_KEY
 }).startRTM()
 
-controller.hears(
-  'heeey!', ['direct_message', 'direct_mention', 'mention'],
+controller.hears('heeey!', ['direct_message', 'direct_mention', 'mention'],
   function (bot, message) {
     bot.reply(message, 'hooo!')
-  }
-)
+  });
+
+controller.hears('calculate (.*)', ['direct_message', 'direct_mention', 'mention'],
+  function (bot, message) {;
+    var cal = message.match[1];
+    var calc = cal.split("");
+    var result = eval(calc.join(''));
+    bot.reply(message, result.toString());
+  });
