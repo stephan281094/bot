c46f775431a7966f8eadac5f895cf2d492ca9e49
Roy <debessjes@hotmail.com>
2016-08-25 11:12:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,18 @@ controller.spawn({
   token: process.env.BOT_API_KEY || process.env.npm_config_BOT_API_KEY
 }).startRTM()
 
-controller.hears(
-  'heeey!', ['direct_message', 'direct_mention', 'mention'],
+controller.hears('heeey!', ['direct_message', 'direct_mention', 'mention'],
   function (bot, message) {
     bot.reply(message, 'hooo!')
   }
 )
 
-controller.hears(
-  'calc(.*)', ['direct_message', 'direct_mention', 'mention'],
+controller.hears('calc(.*)', ['direct_message', 'direct_mention', 'mention'],
   function (bot, message) {
     var calculation = message.match[1]
 
     if (calculation) {
-      // TODO: Sanitize calculation
+      calculation = calculation.replace(/[^-\d/*+.]/g, '');
       var answer = eval(calculation)
       bot.reply(message, 'The answer is ' + answer + '!')
     } else {
